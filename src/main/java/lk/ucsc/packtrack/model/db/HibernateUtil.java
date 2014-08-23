b73b5716e8a1de05d26524ab15336b69053c88e3
paarandika <paarandika@gmail.com>
2014-08-23 12:51:40 +0530

    Changed web-api to 7. Added HibernateUtil.java

---
+++ src/main/java/lk/ucsc/packtrack/model/db/HibernateUtil.java
@@ -0,0 +1,28 @@
+package lk.ucsc.packtrack.model.db;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+
+
+public class HibernateUtil {
+
+	static StandardServiceRegistryBuilder builder;
+	private static SessionFactory sessionFactory;
+	static {
+		try {
+			Configuration configuration = new Configuration().configure();
+			builder = new StandardServiceRegistryBuilder()
+					.applySettings(configuration.getProperties());
+			sessionFactory = configuration.buildSessionFactory(builder.build());
+		} catch (HibernateException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+}
