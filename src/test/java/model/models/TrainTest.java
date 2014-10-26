16d829da1fd972b506966ac5b63fe2f101abb104
Lahiru91 <lahiru.rangana77@gmail.com>
2014-10-26 11:55:27 +0530

    fix some errors in TrainTest.java

--- src/test/java/model/models/TrainTest.java
+++ src/test/java/model/models/TrainTest.java
@@ -10,8 +10,52 @@ public class TrainTest {
 	Station station1=new Station();
 	Station station2=new Station();
 	
-	Train trainDefault=new Train();
-	Train trainParameter=new Train(1, "Podi_Manike", "Rand", station1, station2);
+	Train train=new Train();
+	//Train trainParameter=new Train(1, "Podi_Manike", "Rand", station1, station2);
+	
+	@Test
+	public String getTrain_num() {
+		return train_num;
+	}
+	public void setTrain_num(String train_num) {
+		this.train_num = train_num;
+	}
+	public String getTrain_name() {
+		return train_name;
+	}
+	public void setTrain_name(String train_name) {
+		this.train_name = train_name;
+	}
+	public String getStart_station() {
+		return start_station;
+	}
+	public void setStart_station(String start_station) {
+		this.start_station = start_station;
+	}
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+	public String getTrain_type() {
+		return train_type;
+	}
+	public void setTrain_type(String train_type) {
+		this.train_type = train_type;
+	}
+	public String getTrain_run() {
+		return train_run;
+	}
+	public void setTrain_run(String train_run) {
+		this.train_run = train_run;
+	}
+	public String getTrain_direction() {
+		return train_direction;
+	}
+	public void setTrain_direction(String train_direction) {
+		this.train_direction = train_direction;
+	}
 
 	@Test
 	public void getIDTest() {
