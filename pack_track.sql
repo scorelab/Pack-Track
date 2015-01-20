d5c1a834a037bf76dc78ffe0675a2be07d1364c6
Lahiru91 <lahiru.rangana77@gmail.com>
2015-01-20 16:22:16 +0530

    New Data added

--- pack_track.sql
+++ pack_track.sql
@@ -1,33 +1,13 @@
--- phpMyAdmin SQL Dump
--- version 4.0.4
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Jan 15, 2015 at 07:02 AM
--- Server version: 5.6.12-log
--- PHP Version: 5.4.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
---
--- Database: `pack_track`
---
-
 CREATE DATABASE IF NOT EXISTS `pack_track` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `pack_track`;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `category`
---
 
 DROP TABLE IF EXISTS `category`;
 CREATE TABLE IF NOT EXISTS `category` (
@@ -42,21 +22,12 @@ CREATE TABLE IF NOT EXISTS `category` (
   UNIQUE KEY `catName_UNIQUE` (`catName`),
   KEY `categoryAdmin_idx` (`addBy`),
   KEY `categoryDeleteBy_idx` (`deleteBy`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
-
---
--- Dumping data for table `category`
---
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
 
 INSERT INTO `category` (`catID`, `catName`, `unitCost`, `addBy`, `deleted`, `deleteBy`) VALUES
-(1, 'Normal', 12, 'Rand', 0, NULL),
-(2, 'Furniture', 5.5, 'Rand', 0, NULL);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `customer`
---
+(1, 'Normal', 1, 'Rand', 0, NULL),
+(2, 'Furniture', 5.5, 'Rand', 0, NULL),
+(3, 'Flowers', 1.5, 'Rand', 0, NULL);
 
 DROP TABLE IF EXISTS `customer`;
 CREATE TABLE IF NOT EXISTS `customer` (
@@ -76,29 +47,12 @@ CREATE TABLE IF NOT EXISTS `customer` (
   KEY `customerAdmin_idx` (`addBy`),
   KEY `custAddUser_idx` (`addBy`),
   KEY `custDeleteBy_idx` (`deleteBy`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
-
---
--- Dumping data for table `customer`
---
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
 
 INSERT INTO `customer` (`custID`, `custName`, `custEmail`, `custMobile`, `custTel`, `custAddress`, `custNIC`, `addBy`, `deleted`, `deleteBy`) VALUES
-(1, 'boo', 'booo', '456', '456', 'fgghfhdf', 'dfgdfg4', 'Rand', 0, NULL),
-(2, 'ghfgh', NULL, '', '', NULL, '564', 'Rand', 0, NULL),
-(3, 'poo', NULL, '', '', NULL, '12345', 'Rand', 0, NULL),
-(4, 'fgdfgdf', NULL, '', '', NULL, '546546v', 'Rand', 0, NULL),
-(5, 'nimal', NULL, '1111', '', NULL, '234', 'Rand', 0, NULL),
-(6, 'Ayantha', 'paarandika@gmail.com', '', '', NULL, 'qwe', 'Rand', 0, NULL),
-(7, 'Randika', NULL, '', '', NULL, 'asd', 'Rand', 0, NULL),
-(8, 'fgrff', NULL, '', '', NULL, '12345667v', 'Rand', 0, NULL),
-(9, 'wertdfe', NULL, '', '', NULL, '45334667v', 'Rand', 0, NULL),
-(10, 'ertert', NULL, '', '', NULL, '1235b', 'Rand', 0, NULL);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `device`
---
+(13, 'Rashendra Oshanda', 'lahiru.rangana58@gmail.com', '0714578963', '0112856947', '15, Kadawatha, Kirillawala', '893456752V', 'Thamali', 0, NULL),
+(14, 'Nipuna Udara', 'lahiru.rangana54@gmail.com', '0715489632', '0112587431', '25, Pannipitiya, Kottawa.', '811927356V', 'Thamali', 0, NULL),
+(15, 'Dinka Mandula', 'lahiru.rangana58@gmail.com', '0719547862', '0115463218', '84, Aluthgama', '905765555V', 'Thamali', 0, NULL);
 
 DROP TABLE IF EXISTS `device`;
 CREATE TABLE IF NOT EXISTS `device` (
@@ -114,20 +68,10 @@ CREATE TABLE IF NOT EXISTS `device` (
   KEY `FAdmin_idx` (`addBy`),
   KEY `deviceUser_idx` (`assignTo`),
   KEY `deviceDeleteBy_idx` (`deleteBy`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `device`
---
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 INSERT INTO `device` (`devID`, `imei`, `assignTo`, `addBy`, `deleted`, `deleteBy`) VALUES
 (1, '1234567890', 'Rand', 'Rand', 0, NULL);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `parcel`
---
 
 DROP TABLE IF EXISTS `parcel`;
 CREATE TABLE IF NOT EXISTS `parcel` (
@@ -156,51 +100,17 @@ CREATE TABLE IF NOT EXISTS `parcel` (
   KEY `current_idx` (`currentStation`),
   KEY `assignTrain_idx` (`train`),
   KEY `releaseByUser_idx` (`releaseBy`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=43 ;
-
---
--- Dumping data for table `parcel`
---
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=48 ;
 
 INSERT INTO `parcel` (`parcelID`, `weight`, `currentStation`, `sender`, `receiver`, `starts`, `destination`, `totalCost`, `express`, `dateReceived`, `dateDelivered`, `addBy`, `train`, `released`, `releaseBy`) VALUES
-(5, 34.2, 6, 1, 1, 1, 1, 345.8, '0', 20140916000000, 0, 'Rand', NULL, 1, 'Rand'),
-(6, 45, 1, 2, 2, 1, 13, 2700, '0', 1412596130, 0, 'Rand', NULL, 0, NULL),
-(7, 34, 1, 3, 3, 1, 23, 2040, '1', 1412867498, 0, 'Rand', '8039', 0, NULL),
-(8, 2, 1, 5, 3, 1, 28, 120, '0', 1412960861, 0, 'Rand', NULL, 0, NULL),
-(9, 456, 1, 2, 2, 1, 22, 27360, '0', 1413358072, 0, 'Rand', NULL, 0, NULL),
-(10, 44, 1, 2, 2, 1, 22, 2640, '0', 1413374378, 0, 'Rand', NULL, 0, NULL),
-(11, 2, 1, 3, 3, 1, 16, 120, '0', 1413374495, 0, 'Rand', NULL, 0, NULL),
-(12, 2, 1, 3, 3, 1, 15, 120, '0', 1413374589, 0, 'Rand', NULL, 0, NULL),
-(13, 4, 1, 3, 2, 1, 19, 240, '0', 1413374788, 0, 'Rand', NULL, 0, NULL),
-(14, 3, 1, 2, 2, 1, 7, 180, '0', 1413374990, 0, 'Rand', NULL, 0, NULL),
-(15, 1, 1, 2, 2, 1, 22, 60, '0', 1413375398, 0, 'Rand', NULL, 0, NULL),
-(18, 6, 1, 2, 2, 1, 22, 360, '0', 1413386640, 0, 'Rand', NULL, 0, NULL),
-(19, 2, 1, 2, 2, 1, 5, 120, '0', 1413395509, 0, 'Rand', NULL, 0, NULL),
-(20, 1, 1, 3, 2, 1, 21, 60, '0', 1413395778, 0, 'Rand', NULL, 0, NULL),
-(21, 1, 1, 6, 7, 1, 17, 60, '0', 1413396830, 0, 'Rand', NULL, 0, NULL),
-(22, 3, 1, 6, 7, 1, 19, 180, '0', 1413397141, 0, 'Rand', NULL, 0, NULL),
-(23, 1, 1, 7, 6, 1, 21, 60, '0', 1413397309, 0, 'Rand', NULL, 0, NULL),
-(24, 3, 1, 6, 2, 1, 20, 180, '0', 1413397463, 0, 'Rand', NULL, 0, NULL),
-(25, 67, 1, 2, 6, 1, 20, 4020, '0', 1413397601, 0, 'Rand', NULL, 0, NULL),
-(39, 1, 1, 3, 3, 1, 5, 0, '0', 1414337630, 0, 'Rand', NULL, 0, NULL),
-(40, 1, 1, 3, 3, 1, 5, 136.8, '0', 1414345831, 0, 'Rand', NULL, 0, NULL),
-(41, 1.2, 1, 3, 3, 1, 8, 331.2, '0', 1414347964, 0, 'Rand', NULL, 0, NULL),
-(42, 23, 1, 3, 2, 1, 4, 2566.8, '0', 1414350430, 0, 'Rand', NULL, 0, NULL);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `station_dist`
---
+(45, 10, 21, 13, 14, 7, 21, 3429.8, '0', 1421588082, 0, 'Thamali', NULL, 1, 'Lahiru'),
+(46, 15, 13, 13, 15, 7, 17, 658.5, '0', 1421588296, 0, 'Thamali', NULL, 0, NULL),
+(47, 20, 4, 14, 15, 4, 28, 2135.6, '0', 1421750265, 0, 'Dilanka', NULL, 0, NULL);
 
 DROP TABLE IF EXISTS `station_dist`;
 CREATE TABLE IF NOT EXISTS `station_dist` (
   `dist` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `station_dist`
---
 
 INSERT INTO `station_dist` (`dist`) VALUES
 ('MDA FOT 2.08'),
@@ -235,12 +145,6 @@ INSERT INTO `station_dist` (`dist`) VALUES
 ('WLM KMG 8.5'),
 ('KMG MTR 5.36');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `station_mapping`
---
-
 DROP TABLE IF EXISTS `station_mapping`;
 CREATE TABLE IF NOT EXISTS `station_mapping` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -250,10 +154,6 @@ CREATE TABLE IF NOT EXISTS `station_mapping` (
   UNIQUE KEY `station_code` (`station_code`),
   UNIQUE KEY `station_code_2` (`station_code`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=36 ;
-
---
--- Dumping data for table `station_mapping`
---
 
 INSERT INTO `station_mapping` (`id`, `station_code`, `station_name`) VALUES
 (1, 'MDA', 'Maradana'),
@@ -289,12 +189,6 @@ INSERT INTO `station_mapping` (`id`, `station_code`, `station_name`) VALUES
 (34, 'KMG', 'Kamburugamuwa'),
 (35, 'MTR', 'Matara');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `train_mapping`
---
-
 DROP TABLE IF EXISTS `train_mapping`;
 CREATE TABLE IF NOT EXISTS `train_mapping` (
   `train_num` varchar(45) NOT NULL,
@@ -306,10 +200,6 @@ CREATE TABLE IF NOT EXISTS `train_mapping` (
   `train_direction` varchar(15) DEFAULT NULL,
   PRIMARY KEY (`train_num`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `train_mapping`
---
 
 INSERT INTO `train_mapping` (`train_num`, `start_station`, `destination`, `train_type`, `train_run`, `train_name`, `train_direction`) VALUES
 ('8039', 'MTR', 'KDT', 'PGR', 'EXP', 'Matara-Kandy Ex', 'UP'),
@@ -338,12 +228,6 @@ INSERT INTO `train_mapping` (`train_num`, `start_station`, `destination`, `train
 ('8765', 'MDA', 'KTS', 'PGR', 'EXP', 'Maradana-Kalutara South Express', 'DOWN'),
 ('8766', 'MDA', 'MTR', 'PGR', 'EXP', 'Maradana-Matara Express', 'DOWN');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `UserName` varchar(100) NOT NULL DEFAULT '',
@@ -361,22 +245,14 @@ CREATE TABLE IF NOT EXISTS `user` (
   KEY `userAddBy_idx` (`addBy`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `user`
---
-
 INSERT INTO `user` (`UserName`, `password`, `designation`, `role`, `home_page`, `shed`, `sub_dept`, `nic_number`, `addBy`) VALUES
-('Lahiru', 'd74ff0ee8da3b9806b18c877dbf29bbde50b5bd8e4dad7a3a725000feb82e8f1', 'sith', 'pith', 'gdgd', 'ddd', 'dd', '123456789V', 'Rand'),
-('prrr', '243bff0354584bb553af85db244b05ea9ebb7000af821097fb9f59b75a8480cb', 'ert', 'erty', NULL, 'ert', 'fgh', '123456778v', 'Rand'),
-('Rand', 'd74ff0ee8da3b9806b18c877dbf29bbde50b5bd8e4dad7a3a725000feb82e8f1', 'jil', 'bil', 'gdg', 'gggg', 'gg', '911672375V', 'Rand'),
-('randika', '7b7ab5d581741d0cffe4aab3ec359852537568886e1cd8cb0ccd68bd0397561b', 'Mr', 'SM', NULL, 'Col', 'col', '9116723752V', 'Rand'),
-('thamali', 'fcba89201226b0bc0feac29ed8a07a20559a62c5ea776042fd15877d4b301387', 'Ms', 'SM', NULL, 'No1', '', '12345v', 'Rand');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user_detail`
---
+('Dilanka', 'd74ff0ee8da3b9806b18c877dbf29bbde50b5bd8e4dad7a3a725000feb82e8f1', 'Dilanka_des', 'Dilanka_role', NULL, 'Dilanka_shed', 'Dilanka_sub', '981672375V', 'Rand'),
+('Harsha', 'd74ff0ee8da3b9806b18c877dbf29bbde50b5bd8e4dad7a3a725000feb82e8f1', 'Hrasha_des', 'Harsha_role', NULL, 'Harsha_shed', 'Harsha_sub', '917435776V', 'Rand'),
+('Lahiru', 'd74ff0ee8da3b9806b18c877dbf29bbde50b5bd8e4dad7a3a725000feb82e8f1', 'Lahiru_des', 'Lahiru_role', NULL, 'Lahiru_shed', 'Lahiru-sub', '991672375V', 'Rand'),
+('Maneesha', '06649db4e8efed4b8d40390f760dce37d912cbba9298a3662d45c1561e996e56', 'Maneesha_des', 'MAneesha_role', NULL, 'Maneesha_shed', 'Maneesha_sub', '961672375V', 'Rand'),
+('prabodha', 'fcc5fdcf01f4c8b52b7a63bc992b7549cef6fbe0fe120727aa9a359c82afe3c0', 'Prabodha_des', 'Pabodha_role', NULL, 'Prabodha_shed', 'Prodha_sub', '916250606v', 'Lahiru'),
+('Rand', 'd74ff0ee8da3b9806b18c877dbf29bbde50b5bd8e4dad7a3a725000feb82e8f1', 'Rand_des', 'Rand_role', 'gdg', 'Rand_shed', 'Rand_sub', '911672375V', 'Rand'),
+('Thamali', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'Thamali_des', 'Thamali_role', NULL, 'Thamali_shed', 'Thamali_sub', '947173856V', 'Rand');
 
 DROP TABLE IF EXISTS `user_detail`;
 CREATE TABLE IF NOT EXISTS `user_detail` (
@@ -393,22 +269,14 @@ CREATE TABLE IF NOT EXISTS `user_detail` (
   KEY `deleteByUser_idx` (`deleteBy`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `user_detail`
---
-
 INSERT INTO `user_detail` (`UserName`, `name`, `Email`, `station`, `phone`, `deleted`, `deleteBy`) VALUES
-('Lahiru', 'Rangana', '1ahiru.rangana54@gmail.com', 1, '754129659', 0, NULL),
-('prrr', 'prreww', 'acd@bbb.com', 1, '3456', 0, NULL),
-('Rand', 'Randika', 'lahiru.rangana77@gmail.com', 22, '713823479', 0, NULL),
-('randika', 'rand', 'paarandika@gmail.com', 3, '0716354795', 0, NULL),
-('thamali', 'Thamali', 'ravingae@gmail.com', 1, '1245', 0, NULL);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user_privilege`
---
+('Dilanka', 'Dilanka Wagachchi', 'lahiru.rangana58@gmail.com', 4, '0713823479', 0, NULL),
+('Harsha', 'Harsha Dulshan', 'lahiru.rangana54@gmail.com', 13, '0112054468', 0, NULL),
+('Lahiru', 'Lahiru Rangana', 'lahiru.xmail@gmail.com', 21, '0754129659', 0, NULL),
+('Maneesha', 'Maneesha Erandi', 'harith9127@gmail.com', 17, '0713823479', 0, NULL),
+('prabodha', 'Prabodha Gamage', 'madhushanikapi@gmail.com', 28, '0116789456', 0, NULL),
+('Rand', 'Randika Ayantha', 'lahiru.rangana77@gmail.com', 1, '713823479', 0, NULL),
+('Thamali', 'Thamali Ravinga', 'lahiru.rangana56@gmail.com', 7, '0754129659', 0, NULL);
 
 DROP TABLE IF EXISTS `user_privilege`;
 CREATE TABLE IF NOT EXISTS `user_privilege` (
@@ -435,40 +303,23 @@ CREATE TABLE IF NOT EXISTS `user_privilege` (
   KEY `userPrivilige_idx` (`User_Name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `user_privilege`
---
-
 INSERT INTO `user_privilege` (`User_Name`, `add_user`, `remove_User`, `add_station`, `remove_station`, `add_train`, `remove_train`, `add_category`, `remove_category`, `add_customer`, `remove_customer`, `add_parcel`, `release_parcel`, `confirm_arrival`, `check_upcoming_parcel`, `select_train`, `add_device`, `remove_device`, `accounts`) VALUES
-('Lahiru', 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-('prrr', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+('Dilanka', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0),
+('Harsha', 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0),
+('Lahiru', 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0),
+('Maneesha', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0),
+('prabodha', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0),
 ('Rand', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
-('randika', 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0),
-('thamali', 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+('Thamali', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0);
 
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `category`
---
 
 ALTER TABLE `category`
   ADD CONSTRAINT `categoryAddBy` FOREIGN KEY (`addBy`) REFERENCES `user` (`UserName`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `categoryDeleteBy` FOREIGN KEY (`deleteBy`) REFERENCES `user` (`UserName`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Constraints for table `customer`
---
-
 ALTER TABLE `customer`
   ADD CONSTRAINT `custAddBy` FOREIGN KEY (`addBy`) REFERENCES `user` (`UserName`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `custDeleteBy` FOREIGN KEY (`deleteBy`) REFERENCES `user` (`UserName`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Constraints for table `device`
---
 
 ALTER TABLE `device`
   ADD CONSTRAINT `deviceAddBy` FOREIGN KEY (`addBy`) REFERENCES `user` (`UserName`) ON UPDATE CASCADE,
